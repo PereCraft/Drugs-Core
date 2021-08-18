3454a9867ccb88952ecb13518159c8ca6e3340b8
open-antux <antonio.giustino11@gmail.com>
2021-08-18 23:43:44 +0200

    Modificata gestione delle droghe

--- Drugs-Core/src/perecraft/drugscore/listeners/DrugConsumeListener.java
+++ Drugs-Core/src/perecraft/drugscore/listeners/DrugConsumeListener.java
@@ -12,6 +12,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
+import perecraft.drugscore.domain.Drug;
 
 import perecraft.drugscore.manager.DrugsManager;
 
@@ -30,32 +31,30 @@ public class DrugConsumeListener implements Listener {
                  */
                 ItemStack item = e.getItem();
                 
-                if(item.getItemMeta().hasLocalizedName() && DrugsManager.checkDrug(item.getItemMeta().getLocalizedName())) {
+                if(item.getItemMeta().hasLocalizedName()) {
                     String name = item.getItemMeta().getLocalizedName();
 
-                    Collection<PotionEffect> effects = DrugsManager.getEffects(name);
-                    if(effects == null) return;
+                    if(DrugsManager.getManager().getDrug(name) == null) return;
+                    Drug d = DrugsManager.getManager().getDrug(name);
                     
-                    if(DrugsManager.getDependencies(name) == null) {
-                        return;
-                    } else if(!DrugsManager.getDependencies(name).isEmpty()) {
+                    if(!d.getDependencies().isEmpty()) {
                         
                         // Controllo che l'utente abbia le dipendenze per il consumo
-                        for(Material dep : DrugsManager.getDependencies(name)) {
+                        for(Material dep : d.getDependencies()) {
                             if(p.getInventory().first(dep) == -1)
                                 return;
                         }
                         
                         // Controllo che non sia già sotto effetti
-                        for(PotionEffect pe : effects) {
+                        for(PotionEffect pe : d.getEffects()) {
                             if(p.hasPotionEffect(pe.getType())) {
-                                p.sendMessage(DrugsManager.getWarningMessage());
+                                p.sendMessage(DrugsManager.getManager().getWarningMessage());
                                 return;
                             }
                         }
                         
                         // Consumo materiali
-                        for(Material dep : DrugsManager.getDependencies(name)) {
+                        for(Material dep : d.getDependencies()) {
                             ItemStack itemdep = p.getInventory().getContents()[p.getInventory().first(dep)];
                             
                             if((short)(itemdep.getDurability() + 1) > dep.getMaxDurability()) {
@@ -68,8 +67,8 @@ public class DrugConsumeListener implements Listener {
                         
                         // Particelle
                         Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
-                        if(DrugsManager.getParticle(name) != null) {
-                            Particle part = DrugsManager.getParticle(name);
+                        if(d.getParticle() != null) {
+                            Particle part = d.getParticle();
                             Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(part, p.getLocation(), 40);
                         }
 
@@ -77,15 +76,15 @@ public class DrugConsumeListener implements Listener {
                         // Give degli effetti
                         item.setAmount(item.getAmount() - 1);
                         
-                        p.addPotionEffects(effects);
-                        p.playSound(p.getLocation(), DrugsManager.getSound(name), 10, 1);
+                        p.addPotionEffects(d.getEffects());
+                        p.playSound(p.getLocation(), d.getSound(), 10, 1);
                         return;
                                                 
                     }
                     
-                    for(PotionEffect pe : effects) {
+                    for(PotionEffect pe : d.getEffects()) {
                         if(p.hasPotionEffect(pe.getType())) {
-                            p.sendMessage(DrugsManager.getWarningMessage());
+                            p.sendMessage(DrugsManager.getManager().getWarningMessage());
                             return;
                         }
                     }
@@ -93,101 +92,27 @@ public class DrugConsumeListener implements Listener {
                     Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
                     item.setAmount(item.getAmount() - 1);
                     
-                    p.addPotionEffects(effects);
-                    p.playSound(p.getLocation(), DrugsManager.getSound(name), 10, 1);
+                    p.addPotionEffects(d.getEffects());
+                    p.playSound(p.getLocation(), d.getSound(), 10, 1);
                     
                 }                
 
                 /**
                  * Pianta
                  */
+                // TODO
             }
         }
 
-//        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-//            Player p = e.getPlayer();
-//
-//            if(p.isSneaking()) {
-//
-//                Block clickedblock = e.getClickedBlock();
-//
-//                if(clickedblock != null && clickedblock.hasMetadata("drug-plant")) {
-//                    e.setCancelled(true);
-//                    checkGrowth(p, clickedblock);
-//                    return;
-//                }
-//
-//                ItemStack item = e.getItem();
-//                String cooldown = "§cVacci piano bello!";
-//
-//                if(item == null) return;
-//
-//                // TODO: Automatizzare processo di controllo e consumo dell'oggetto
-//                if(item.getType() == Material.SUGAR && item.getItemMeta().getDisplayName().contains(DrugsManager.cocainename)) {
-//                    if(p.hasPotionEffect(PotionEffectType.SPEED)) {
-//                        p.sendMessage(cooldown);
-//                        return;
-//                    }
-//
-//                    e.setCancelled(true);
-//                    
-//                    int amount = item.getAmount();
-//                    item.setAmount(amount-1);
-//
-//                    p.updateInventory();
-//                    p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1));
-//                    p.playSound(p.getLocation(), Sound.ENTITY_CAT_HISS, 10, 1);
-//                    p.sendMessage("§7Hai assunto la coca");
-//                } else if(item.getType() == Material.INK_SACK && item.getItemMeta().getDisplayName().contains(DrugsManager.weedname)) {
-//                    e.setCancelled(true);
-//
-//                    if(p.hasPotionEffect(PotionEffectType.REGENERATION)) {
-//                        p.sendMessage(cooldown);
-//                        return;
-//                    }
-//
-//                    if(p.getInventory().contains(Material.FLINT_AND_STEEL) && p.getInventory().contains(Material.PAPER)) {
-//                        for(ItemStack slot : p.getInventory().getStorageContents()) {
-//                            if(slot.getType()==Material.PAPER) {
-//                                int paperamount = slot.getAmount();
-//                                slot.setAmount(paperamount-1);
-//
-//                                int weedamount = item.getAmount();
-//                                item.setAmount(weedamount-1);
-//
-//                                p.updateInventory();
-//                                p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 2));
-//                                p.addPotionEffect(new PotionEffect(PotionEffectType.HUNGER, 400, 1));
-//                                Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.SMOKE_LARGE, p.getLocation(), 40);
-//                                p.playSound(p.getLocation(), Sound.BLOCK_FIRE_AMBIENT, 10, 1);
-//                                p.sendMessage("§7Hai fumato la weed");
-//
-//                                break;
-//                            }
-//                        }
-//
-//                    } else {
-//                        p.sendMessage("§7Devi avere un accendino e della carta per fumare la weed!");
-//                    }
-//
-//                }
-//
-//            }
-//
-//            // TODO: Da aggiungere controllo dell'oggetto che si sta usando sulla 
-//            // mano, se è una droga → consuma, es:
-//            // if(!DrugsManager.isADrug(e.getPlayer.getItemInMainHand()) return;
-//
-//        }
-
     }
 
-    private void checkGrowth(Player p, Block plant) {
+    // TODO
+//    private void checkGrowth(Player p, Block plant) {
 //        if(plant.getMetadata("drug-plant").get(0).asString() == "cocaine-plant-1") {
 //            p.sendMessage("§7Questa pianta di coca sta crescendo...");
 //        } else if(plant.getMetadata("drug-plant").get(0).asString() == "cocaine-plant-2") {
 //            p.sendMessage("§aPianta di coca cresciuta!");
 //        }
-    }
+//    }
 	
 }
