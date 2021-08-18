126916a1ddb70fda6c9d5f034af8a27de95c10c5
open-antux <antonio.giustino11@gmail.com>
2021-08-18 23:44:06 +0200

    Prima versione del MobListener generalizzato

--- Drugs-Core/src/perecraft/drugscore/listeners/MobListener.java
+++ Drugs-Core/src/perecraft/drugscore/listeners/MobListener.java
@@ -1,12 +1,17 @@
 package perecraft.drugscore.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import perecraft.drugscore.domain.CustomDrop;
+import perecraft.drugscore.domain.Drug;
+import perecraft.drugscore.domain.Seed;
 
 import perecraft.drugscore.manager.DrugsManager;
 
@@ -18,17 +23,36 @@ public class MobListener implements Listener {
      */
     @EventHandler
     public void onEntityDeath(EntityDeathEvent e) {
-
-//        if(e.getEntity().getType()==EntityType.ZOMBIE) {
-//            ItemStack cocaineitem = new ItemStack(Material.PUMPKIN_SEEDS);
-//            ItemMeta cocainemeta = cocaineitem.getItemMeta();
-//
-//            cocainemeta.setDisplayName(DrugsManager.cocaineseedname);
-//            cocainemeta.setLore(DrugsManager.cocaineseedlore);
-//            cocaineitem.setItemMeta(cocainemeta);
-//
-//            e.getDrops().add(1, cocaineitem);
-//        }
+        
+        // FIX: Crasha quando override-drop è true
+        if(DrugsManager.getManager().getCustomDrop(e.getEntity().getType()) != null) {
+            CustomDrop cd = DrugsManager.getManager().getCustomDrop(e.getEntity().getType());
+            
+            // FIX
+            if(cd.getOverrideDrop()) {
+                System.out.println("Drops: " + e.getDrops());
+                e.getDrops().clear();
+            }
+            
+            // Drugs chance
+            if(!cd.getDrugsList().isEmpty() && cd.getDrugsChance() != 0) {
+                if(Math.round(Math.random()*100)%(100/cd.getDrugsChance()) == 0) {
+                    cd.getDrugsList().forEach((Drug d) -> {
+                        e.getDrops().add(1, d.getItem(1));
+                    });
+                }
+            }            
+            
+            // Seeds chance
+            if(!cd.getSeedsList().isEmpty() && cd.getSeedsChance() != 0) {
+                if(Math.round(Math.random()*100)%(100/cd.getSeedsChance()) == 0) {
+                    cd.getSeedsList().forEach((Seed s) -> {
+                        e.getDrops().add(1, s.getItem(1));
+                    });
+                }
+            }
+            
+        }
 
     }
 	
