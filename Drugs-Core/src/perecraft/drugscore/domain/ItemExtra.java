f78bb2ed310a01bb273c366c09b6c73b62fc4c3e
open-antux <antonio.giustino11@gmail.com>
2021-09-22 22:38:57 +0200

    Aggiunto ItemExtra

---
+++ Drugs-Core/src/perecraft/drugscore/domain/ItemExtra.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perecraft.drugscore.domain;
+
+import java.util.List;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ *
+ * @author PereCraft
+ */
+public class ItemExtra {
+    private String id;
+    private Material material;
+    private Short shortnum;
+    private String displayName;
+    private List<String> lore;
+
+    public ItemExtra() {}
+
+    public ItemExtra(String id, String material, Short shortnum, String displayName, List<String> lore) {
+        this.id = id;
+        this.material = Material.matchMaterial(material);
+        this.shortnum = shortnum;
+        this.displayName = displayName;
+        this.lore = lore;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public Short getShortnum() {
+        return shortnum;
+    }
+
+    public void setShortnum(Short shortnum) {
+        this.shortnum = shortnum;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public List<String> getLore() {
+        return lore;
+    }
+
+    public void setLore(List<String> lore) {
+        this.lore = lore;
+    }
+    
+    public void giveItem(Player target, int amount) {
+        ItemStack item = new ItemStack(material, amount, shortnum);
+        ItemMeta meta = item.getItemMeta();
+        
+        meta.setLocalizedName(id);
+        meta.setDisplayName(displayName);
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        
+        target.getInventory().addItem(item);
+        target.updateInventory();
+    }
+    
+    public ItemStack getItem(int amount) {
+        ItemStack item = new ItemStack(material, amount, shortnum);
+        ItemMeta meta = item.getItemMeta();
+        
+        meta.setLocalizedName(id);
+        meta.setDisplayName(displayName);
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+        
+        return item;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\n " +
+                "Display name: " + displayName + "\n " +
+                "Material: " + material.toString() + "\n " +
+                "Short number: " + shortnum + "\n " +
+                "Lore: " + String.join(" - ", lore); 
+    }            
+}
