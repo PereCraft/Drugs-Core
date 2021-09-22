8dcc83afc3e11bc08ec25ab02b17c6d1f438ba6a
open-antux <antonio.giustino11@gmail.com>
2021-09-22 22:39:47 +0200

    Riscritto metodo per consumo delle droghe

--- Drugs-Core/src/perecraft/drugscore/listeners/DrugConsumeListener.java
+++ Drugs-Core/src/perecraft/drugscore/listeners/DrugConsumeListener.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +14,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import perecraft.drugscore.domain.Drug;
+import perecraft.drugscore.domain.ItemExtra;
 
 import perecraft.drugscore.manager.DrugsManager;
 
@@ -41,51 +43,30 @@ public class DrugConsumeListener implements Listener {
                     if(DrugsManager.getManager().getDrug(name) == null) return;
                     Drug d = DrugsManager.getManager().getDrug(name);
                     
+                    // Controllo che l'utente abbia le dipendenze per il consumo
                     if(!d.getDependencies().isEmpty()) {
-                        
-                        // Controllo che l'utente abbia le dipendenze per il consumo
                         for(Material dep : d.getDependencies()) {
                             if(p.getInventory().first(dep) == -1)
                                 return;
                         }
-                        
-                        // Controllo che non sia già sotto effetti
-                        for(PotionEffect pe : d.getEffects()) {
-                            if(p.hasPotionEffect(pe.getType())) {
-                                p.sendMessage(DrugsManager.getManager().getWarningMessage());
-                                return;
-                            }
-                        }
-                        
-                        // Consumo materiali
-                        for(Material dep : d.getDependencies()) {
-                            ItemStack itemdep = p.getInventory().getContents()[p.getInventory().first(dep)];
-                            
-                            if((short)(itemdep.getDurability() + 1) > dep.getMaxDurability()) {
-                                itemdep.setAmount(itemdep.getAmount() - 1);
-                            } else {
-                                itemdep.setDurability((short)(itemdep.getDurability() + 1));
-                            }
-                            
-                        }
-                        
-                        // Particelle
-                        Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
-                        if(d.getParticle() != null) {
-                            Particle part = d.getParticle();
-                            Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(part, p.getLocation(), 40);
-                        }
-
-                        
-                        // Give degli effetti
-                        item.setAmount(item.getAmount() - 1);
-                        
-                        p.addPotionEffects(d.getEffects());
-                        p.playSound(p.getLocation(), d.getSound(), 10, 1);
-                        return;
-                                                
                     }
                     
+                    // Controllo che l'utente abbia le dipendenze extra per il consumo
+                    if(!d.getDependenciesExtra().isEmpty()) {
+                        boolean exitStatus = true;
+                        
+                        for(ItemExtra ie : d.getDependenciesExtra()) {                            
+                            for(ItemStack itemdep : p.getInventory().all(ie.getMaterial()).values()) {
+                                if(itemdep.getItemMeta().hasLocalizedName() && itemdep.getItemMeta().getLocalizedName().equals(ie.getId())) {
+                                    exitStatus = false;
+                                } 
+                            }
+                        }
+                        
+                        if(exitStatus) return;
+                    }
+                    
+                    // Controllo che non sia già sotto effetti
                     for(PotionEffect pe : d.getEffects()) {
                         if(p.hasPotionEffect(pe.getType())) {
                             p.sendMessage(DrugsManager.getManager().getWarningMessage());
@@ -93,11 +74,138 @@ public class DrugConsumeListener implements Listener {
                         }
                     }
                     
+                    // Consumo materiali
+                    for(Material dep : d.getDependencies()) {
+                        ItemStack itemdep = p.getInventory().getContents()[p.getInventory().first(dep)];
+
+                        if((short)(itemdep.getDurability() + 1) > dep.getMaxDurability()) {
+                            itemdep.setAmount(itemdep.getAmount() - 1);
+                        } else {
+                            itemdep.setDurability((short)(itemdep.getDurability() + 1));
+                        }
+
+                    }
+                    
+                    // Consumo ItemExtra
+                    for(ItemExtra ie : d.getDependenciesExtra()) {                            
+                        for(ItemStack itemdep : p.getInventory().all(ie.getMaterial()).values()) {
+                            if(!itemdep.getItemMeta().hasLocalizedName() && !itemdep.getItemMeta().getLocalizedName().equals(ie.getId()))  {
+                                System.out.println("sas");
+                                return;
+                            }
+
+                            if(!itemdep.getItemMeta().hasLocalizedName()) return;
+                            if(!itemdep.getItemMeta().getLocalizedName().equals(ie.getId())) return;
+
+                            if((short)(itemdep.getDurability() + 1) > itemdep.getType().getMaxDurability()) {
+                                itemdep.setAmount(itemdep.getAmount() - 1);
+                            } else {
+                                itemdep.setDurability((short)(itemdep.getDurability() + 1));
+                            }
+                        }
+                    }
+
+
+                    // Particelle
+                    Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
+                    if(d.getParticle() != null) {
+                        Particle part = d.getParticle();
+                        Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(part, p.getLocation(), 40);
+                    }
+                    
+                    // Rottura item, give effetti, avvio suono
                     Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
                     item.setAmount(item.getAmount() - 1);
                     
                     p.addPotionEffects(d.getEffects());
                     p.playSound(p.getLocation(), d.getSound(), 10, 1);
+//                    if(!d.getDependencies().isEmpty()) {
+//                        
+//                        // Controllo che l'utente abbia le dipendenze per il consumo
+//                        for(Material dep : d.getDependencies()) {
+//                            if(p.getInventory().first(dep) == -1)
+//                                return;
+//                        }
+//                        
+//                        // Controllo che l'utente abbia le dipendenze extra per il consumo
+////                        boolean exitStatus = true;
+////                        for(ItemExtra ie : d.getDependenciesExtra()) {                            
+////                            for(ItemStack itemdep : p.getInventory().all(ie.getMaterial()).values()) {
+////                                if(itemdep.getItemMeta().hasLocalizedName() && itemdep.getItemMeta().getLocalizedName().equals(ie.getId())) {
+////                                    exitStatus = false;
+////                                } 
+////                            }
+////                        }
+////                        if(exitStatus) return;
+//                                                
+//                        // Controllo che non sia già sotto effetti
+//                        for(PotionEffect pe : d.getEffects()) {
+//                            if(p.hasPotionEffect(pe.getType())) {
+//                                p.sendMessage(DrugsManager.getManager().getWarningMessage());
+//                                return;
+//                            }
+//                        }
+//                        
+//                        // Consumo materiali
+//                        for(Material dep : d.getDependencies()) {
+//                            ItemStack itemdep = p.getInventory().getContents()[p.getInventory().first(dep)];
+//                            
+//                            if((short)(itemdep.getDurability() + 1) > dep.getMaxDurability()) {
+//                                itemdep.setAmount(itemdep.getAmount() - 1);
+//                            } else {
+//                                itemdep.setDurability((short)(itemdep.getDurability() + 1));
+//                            }
+//                            
+//                        }
+//                        
+//                        // Consumo ItemExtra
+//                        for(ItemExtra ie : d.getDependenciesExtra()) {                            
+//                            for(ItemStack itemdep : p.getInventory().all(ie.getMaterial()).values()) {
+//                                if(!itemdep.getItemMeta().hasLocalizedName() && !itemdep.getItemMeta().getLocalizedName().equals(ie.getId()))  {
+//                                    System.out.println("sas");
+//                                    return;
+//                                }
+//
+//                                //if(!itemdep.getItemMeta().hasLocalizedName()) return;
+//                                //if(!itemdep.getItemMeta().getLocalizedName().equals(ie.getId())) return;
+//                                
+//                                if((short)(itemdep.getDurability() + 1) > itemdep.getType().getMaxDurability()) {
+//                                    itemdep.setAmount(itemdep.getAmount() - 1);
+//                                } else {
+//                                    itemdep.setDurability((short)(itemdep.getDurability() + 1));
+//                                }
+//                            }
+//                        }
+//                        
+//                        // Particelle
+//                        Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
+//                        if(d.getParticle() != null) {
+//                            Particle part = d.getParticle();
+//                            Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(part, p.getLocation(), 40);
+//                        }
+//
+//                        
+//                        // Give degli effetti
+//                        item.setAmount(item.getAmount() - 1);
+//                        
+//                        p.addPotionEffects(d.getEffects());
+//                        p.playSound(p.getLocation(), d.getSound(), 10, 1);
+//                        return;
+//                                                
+//                    }
+//                    
+//                    for(PotionEffect pe : d.getEffects()) {
+//                        if(p.hasPotionEffect(pe.getType())) {
+//                            p.sendMessage(DrugsManager.getManager().getWarningMessage());
+//                            return;
+//                        }
+//                    }
+//                    
+//                    Bukkit.getServer().getWorld(p.getWorld().getName()).spawnParticle(Particle.ITEM_CRACK, p.getLocation(), 40, item);
+//                    item.setAmount(item.getAmount() - 1);
+//                    
+//                    p.addPotionEffects(d.getEffects());
+//                    p.playSound(p.getLocation(), d.getSound(), 10, 1);
                     
                 }                
 
